5c8c95e78a6e0a7cb46432701849d8bb16868e14
Brandon Divis <brandon.divis@gmail.com>
2018-11-10 20:26:19 -0600

    Add Grommet UI library

--- src/App.js
+++ src/App.js
@@ -1,37 +1,44 @@
 import React, { Component } from "react";
-import firstnames from './firstNames';
-import lastnames from './lastNames';
+import firstnames from "./firstNames";
+import lastnames from "./lastNames";
+import { Box, Grommet, Heading, Button, Text } from "grommet";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      generatedName: '',
-      gender: '',
-      nationality: ''
+      generatedName: "",
+      gender: "",
+      nationality: ""
     };
   }
 
   onClick = () => {
     const filteredFirstNames = firstnames.filter(name => {
-      return name.gender === this.state.gender
-          && name.nationality === this.state.nationality;
+      return (
+        name.gender === this.state.gender &&
+        name.nationality === this.state.nationality
+      );
     });
 
     const filteredLastNames = lastnames.filter(name => {
       return name.nationality === this.state.nationality;
     });
 
-    const indexFirst = Math.floor(Math.random() * filteredFirstNames.length)
-    const indexLast = Math.floor(Math.random() * filteredLastNames.length)
+    const indexFirst = Math.floor(Math.random() * filteredFirstNames.length);
+    const indexLast = Math.floor(Math.random() * filteredLastNames.length);
 
-    if (this.state.gender !== '' && this.state.nationality !== '') {
+    if (this.state.gender !== "" && this.state.nationality !== "") {
       this.setState({
-        generatedName: "Your name is: " + filteredFirstNames[indexFirst].name + " " + filteredLastNames[indexLast].name
+        generatedName:
+          "Your name is: " + 
+          filteredFirstNames[indexFirst].name +
+          " " +
+          filteredLastNames[indexLast].name
       });
     } else {
-      alert('Enter a value!');
+      alert("Enter a value!");
     }
   };
 
@@ -49,22 +56,27 @@ class App extends Component {
 
   render() {
     return (
-      <div
-        style={{
-          border: "1px solid black",
-          width: "60vw",
-          margin: "auto"
-        }}
+      <Box
+        align="center"
+        elevation="small"
+        width="medium"
+        height="85vh"
+        margin="auto"
+        pad="medium"
+        background="white"
       >
-        <p style={{ fontSize: "40px" }}>Name Generator 4.20</p>
-        <h1>Gender:</h1>
+        <Heading level="2">Name Generator</Heading>
+        <Text size="large" margin="small">What gender would you like?:</Text>
         <select onChange={this.onGenderChange} value={this.state.gender}>
           <option selected />
           <option value="male">Male</option>
           <option value="female">Female</option>
         </select>
-        <h1>Nationality:</h1>
-        <select onChange={this.onNationalityChange} value={this.state.nationality}>
+        <Text size="large" margin="small">What nationality would you like?:</Text>
+        <select
+          onChange={this.onNationalityChange}
+          value={this.state.nationality}
+        >
           <option selected />
           <option value="chinese">Asian</option>
           <option value="japanese">Japanese</option>
@@ -75,9 +87,9 @@ class App extends Component {
           <option value="italian">Italian</option>
           <option value="russian">Russian</option>
         </select>
-        <button onClick={this.onClick}>Generate Name</button>
-        <h1>{this.state.generatedName}</h1>
-      </div>
+        <Button label="Generate name" onClick={this.onClick} margin="medium" />
+        <Heading level="3" textAlign="center">{this.state.generatedName}</Heading>
+      </Box>
     );
   }
 }
